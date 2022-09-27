b248b0a1f789d1d7acf14518be2213b3d77b3f03
Nicole Fazal <108958417+NJFazal@users.noreply.github.com>
2022-09-26 21:56:26 -0400

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,13 +1,10 @@
-
 const balls = document.getElementsByClassName('ball');
 document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
-
-  for (let i = 0; i < 2; i++) {
+  for (var i=0; i<2; i++){
     balls[i].style.left = x;
     balls[i].style.top = y;
-    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+    balls[i].style.transform = 'translate(-' + x + ',-' + y + ')';
   }
 };
-
